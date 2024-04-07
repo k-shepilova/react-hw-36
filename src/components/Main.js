792e81d6b2f6173e1bc1c21ae6f41af25b81b9c9
Kateryna Shepilova <kateryna.shepilova@md.p2h.com>
2024-04-07 23:01:43 +0300

    hw-36

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Main extends React.Component {
+    render(){
+        return (
+            <main>
+                Main Content
+            </main>
+        )
+    }
+}
+
+export default Main;
